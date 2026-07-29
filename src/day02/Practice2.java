e96a9d384d30bf73c0bc2eec6213484f2b2e1038
itdanja2025 <itdanja2025@gmail.com>
2026-07-29 12:42:49 +0900

    day17 3h

--- src/day02/Practice2.java
+++ src/day02/Practice2.java
@@ -17,13 +17,13 @@ public class Practice2 {
         System.out.print("키를 입력하세요: "); double 키 = scan.nextDouble();
         System.out.println( "당신의 키는 약 " + (int)키 + "cm 이군요.");
 
-        // 4. int+int+int/double => double , 입력객체는 클래스내 1개만 존재해도 입력함수 사용가능!
-        System.out.print("1] 정수: "); int 정수1 = scan.nextInt();
+        // 4. int+int+int/double => double , 
+        System.out.print("1] 정수: "); int 정수1 = scan.nextInt(); // 입력객체는 클래스내 1개만 존재해도 입력함수 사용가능!
         System.out.print("2] 정수: "); int 정수2 = scan.nextInt();
         System.out.print("3] 정수: "); int 정수3 = scan.nextInt();
         double result4 = ( 정수1 + 정수2 + 정수3 ) / 3.0; // 3 으로 나누면 소수점 사라진다. 3.0 으로 하여금 자동 double 타입변환 
         System.out.println( result4 );
-        
+
     } // main end 
 } // class end 
 
